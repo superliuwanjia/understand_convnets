718add87d867cdd783fd3477e784f9d14d9a5d37
Owl <mn15@rice.edu>
2017-05-25 12:23:52 -0500

    do conv layers

--- one_conv_layer_2objects_train.py
+++ one_conv_layer_2objects_train.py
@@ -115,7 +115,7 @@ def main():
     # first layer
     ks1 = [250, 250, 1]
     nf1 = 2
-    h_size = nf1 * (input_shape[0] - ks1[0] + 1) * (input_shape[1] - ks1[1] + 1)/4  # Number of hidden nodes 
+    h_size = nf1 * (input_shape[0] - ks1[0] + 1) * (input_shape[1] - ks1[1] + 1)  # Number of hidden nodes
     w_conv1 = init_weights([ks1[0], ks1[1], ks1[2], nf1], name="w1")
     b_conv1 = init_bias([nf1], name="b1")
 
