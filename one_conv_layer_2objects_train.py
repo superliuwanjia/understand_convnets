4779c0fc8de04aa159e9dd6d39748a3f84dafbfc
Owl <mn15@rice.edu>
2017-05-25 12:10:21 -0500

    do conv layers

--- one_conv_layer_2objects_train.py
+++ one_conv_layer_2objects_train.py
@@ -8,7 +8,7 @@ import glob
 bs = 32
 epochs = 4
 image_mode = "L"
-saved_model = "softmax_2objects_RGB.ckpt"
+saved_model = "conv_2objects_RGB.ckpt"
 RANDOM_SEED = 42
 train_test_ratio = 0.8
 input_shape = [250, 250, 1]
