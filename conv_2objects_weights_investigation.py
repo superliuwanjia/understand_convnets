cefd38276667123fb66ab2d5bdf14ef5251cf58f
Owl <mn15@rice.edu>
2017-05-26 00:00:09 -0500

    do conv layers

--- conv_2objects_weights_investigation.py
+++ conv_2objects_weights_investigation.py
@@ -79,7 +79,7 @@ def main():
         # visualize reconstruction from u1
         w_soft_val = sess.run(w_soft)
         w_soft_init_val = sess.run(w_soft_init)
-        I_hat_from_u1_val = sess.run(I_hat_from_u1, feed_dict={X: test_X, y: test_y})
+        I_hat_from_u1_val = sess.run(I_hat_from_u1, feed_dict={X: train_X, y: train_y})
         save_images([I_hat_from_u1_val[i,:,:,:] for i in range(I_hat_from_u1_val.shape[0])], \
                     [str(i) + ".png" for i in range(I_hat_from_u1_val.shape[0])], os.path.join(viz_path, "I_hat_from_u1"), dim=(250,250,1))
 
